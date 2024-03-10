db12d94588e43ae1586257706b6a83cb705a19c3
coatespeter <65430782+coatespeter@users.noreply.github.com>
2024-03-10 07:53:19 +0100

    ready code for deployment

--- automino/settings.py
+++ automino/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5-uu%vr2wg9ri2hanlulqtw%906_r#-du0px(52@sr#geixc^f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
