778fb317df67aa059949099361668a9f73656133
coatespeter <65430782+coatespeter@users.noreply.github.com>
2024-03-11 08:49:08 +0100

    dabatase linked, debus false

--- autonimo/settings.py
+++ autonimo/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5-uu%vr2wg9ri2hanlulqtw%906_r#-du0px(52@sr#geixc^f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'autonimo-662c76ca9bdc.herokuapp.com']
 
