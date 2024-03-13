da5bf3044e91ef7b67fdd7b40be12ef397d084e1
coatespeter <65430782+coatespeter@users.noreply.github.com>
2024-03-13 06:08:55 +0100

    post model created

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+class Post(models.Model):
+
+    CATEGORY_CHOICES = [
+        ('transportation', 'Transportation'),
+        ('land_management', 'Land Management'),
+        ('swimming_pool_management', 'Swimming Pool Management'),
+        ('handyman', 'Handyman'),
+    ]
+
+    title = models.CharField(max_length=200, unique=True)
+    category = models.CharField(max_length=30, choices=CATEGORY_CHOICES)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    content = models.TextField()
+    image = models.ImageField(upload_to='images/')
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=[(0, "Draft"), (1, "Publish")])
+    
+
