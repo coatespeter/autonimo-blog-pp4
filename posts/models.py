1dec294520622f6babd9918b7569956b7eb88ffb
coatespeter <65430782+coatespeter@users.noreply.github.com>
2024-03-13 06:46:40 +0100

    update comment model

--- posts/models.py
+++ posts/models.py
@@ -22,5 +22,19 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=[(0, "Draft"), (1, "Publish")])
     updated_on = models.DateTimeField(auto_now=True)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+    
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return 'Comment {} by {}'.format(self.body, self.name)
     
 
