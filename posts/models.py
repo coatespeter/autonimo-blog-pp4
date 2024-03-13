b82cb15a6a4e631326eb25fdeb51667f8e1212c5
coatespeter <65430782+coatespeter@users.noreply.github.com>
2024-03-13 06:54:21 +0100

    update methods in models.py

--- posts/models.py
+++ posts/models.py
@@ -23,6 +23,12 @@ class Post(models.Model):
     status = models.IntegerField(choices=[(0, "Draft"), (1, "Publish")])
     updated_on = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
+
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -35,6 +41,6 @@ class Comment(models.Model):
         ordering = ['created_on']
 
     def __str__(self):
-        return 'Comment {} by {}'.format(self.body, self.name)
+        return f"Comment {self.body} by {self.author}"
     
 
