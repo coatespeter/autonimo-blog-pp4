4e75d9584db0ff544ce8b4c15b0af6cd89e185ca
coatespeter <65430782+coatespeter@users.noreply.github.com>
2024-03-15 16:37:49 +0100

    cleanup of code files

--- about/forms.py
+++ about/forms.py
@@ -1,7 +1,6 @@
 from .models import CollaborateRequest
 from django import forms
 
-
 class CollaborateForm(forms.ModelForm):
     class Meta:
         model = CollaborateRequest
--- about/models.py
+++ about/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from cloudinary.models import CloudinaryField
 # Create your models here.
 
-
 class About(models.Model):
     title = models.CharField(max_length=200)
     updated_on = models.DateTimeField(auto_now=True)
@@ -10,7 +9,6 @@ class About(models.Model):
 
     def __str__(self):
         return self.title
-
 
 class CollaborateRequest(models.Model):
     name = models.CharField(max_length=200)
