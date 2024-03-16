718420c7d965af2b0c79cfcbbd85075cfe6983ba
coatespeter <65430782+coatespeter@users.noreply.github.com>
2024-03-16 17:25:46 +0100

    formatting

--- posts/models.py
+++ posts/models.py
@@ -20,7 +20,9 @@ class Post(models.Model):
     category = models.CharField(max_length=30, choices=CATEGORY_CHOICES)
     slug = models.SlugField(max_length=200, unique=True)
     featured_image = CloudinaryField('image', default='placeholder')
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='blog_posts'
+        )
     excerpt = models.TextField(blank=True)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
@@ -35,7 +37,9 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='comments'
+        )
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
